7e46a55dc6669cd4f77bf3293563e747b0cab142
xcasadio <casaprod@hotmail.fr>
2026-03-07 23:20:13 +0100

    fix: add [STAThread] to Main to satisfy MGUI STA requirement

--- custom-tools/DbzLegendsAnalyser/DbzLegendsAnalyser/Program.cs
+++ custom-tools/DbzLegendsAnalyser/DbzLegendsAnalyser/Program.cs
@@ -1,2 +1,11 @@
-﻿using var game = new DbzLegendsAnalyser.Game1();
-game.Run();
+﻿using System;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        using var game = new DbzLegendsAnalyser.Game1();
+        game.Run();
+    }
+}
